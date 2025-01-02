525184bfd2be73a88146940b07e8d8c83471ccc6
MrPowerGamerBR <git@mrpowergamerbr.com>
2025-01-01 21:51:14 -0300

    Fix skull command, always pull the texture/signature data from Mojang

--- bukkit/DreamMini/src/main/kotlin/net/perfectdreams/dreammini/commands/SkullCommand.kt
+++ bukkit/DreamMini/src/main/kotlin/net/perfectdreams/dreammini/commands/SkullCommand.kt
@@ -1,46 +1,95 @@
 package net.perfectdreams.dreammini.commands
 
+import com.destroystokyo.paper.profile.ProfileProperty
+import net.kyori.adventure.text.format.NamedTextColor
 import net.perfectdreams.commands.annotation.Subcommand
 import net.perfectdreams.commands.bukkit.SparklyCommand
+import net.perfectdreams.dreamcore.DreamCore
+import net.perfectdreams.dreamcore.utils.adventure.textComponent
+import net.perfectdreams.dreamcore.utils.commands.context.CommandArguments
+import net.perfectdreams.dreamcore.utils.commands.context.CommandContext
+import net.perfectdreams.dreamcore.utils.commands.declarations.SparklyCommandDeclaration
+import net.perfectdreams.dreamcore.utils.commands.declarations.SparklyCommandDeclarationWrapper
+import net.perfectdreams.dreamcore.utils.commands.declarations.sparklyCommand
+import net.perfectdreams.dreamcore.utils.commands.executors.SparklyCommandExecutor
+import net.perfectdreams.dreamcore.utils.commands.options.CommandOptions
 import net.perfectdreams.dreamcore.utils.generateCommandInfo
 import net.perfectdreams.dreammini.DreamMini
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
+import java.util.*
 
-class SkullCommand(val m: DreamMini) : SparklyCommand(arrayOf("skull", "cabeça"), permission = "dreammini.skull"){
+class SkullCommand(val m: DreamMini) : SparklyCommandDeclarationWrapper {
+	override fun declaration() = sparklyCommand(listOf("skull", "cabeça", "head")) {
+		permission = "dreammini.skull"
 
-	@Subcommand
-	fun root(sender: Player){
-		sender.sendMessage(generateCommandInfo("skull <player>"))
+		executor = HeadExecutor(m)
 	}
 
-	@Subcommand
-	fun skull(sender: Player, owner: String){
-		val item = sender.inventory.itemInMainHand
-		val type = item?.type
+	class HeadExecutor(val m: DreamMini) : SparklyCommandExecutor() {
+		inner class Options : CommandOptions() {
+			val name = word("name")
+			val autoUpdate = boolean("auto_update")
+		}
 
-		if (type == Material.PLAYER_HEAD) {
-			// É necessário "clonar" o item, se não for "clonado", não será possível usar "meta.owner" caso a skull já tenha
-			// um owner anterior
-			val skull = ItemStack(Material.PLAYER_HEAD, 1)
-			skull.amount = item.amount
-			skull.addEnchantments(item.enchantments)
-			val meta = item.itemMeta as SkullMeta
-			val skullMeta = skull.itemMeta as SkullMeta
+		override val options = Options()
 
-			skullMeta.addItemFlags(*meta.itemFlags.toTypedArray())
-			skullMeta.setDisplayName(meta.displayName)
-			skullMeta.lore = meta.lore
-			skullMeta.owner = owner
+		override fun execute(context: CommandContext, args: CommandArguments) {
+			val sender = context.requirePlayer()
+			val item = sender.inventory.itemInMainHand
+			val type = item.type
+			val owner = args[options.name]
+			val autoUpdate = args[options.autoUpdate]
 
-			skull.itemMeta = skullMeta
-			sender.inventory.setItemInMainHand(skull)
+			if (type == Material.PLAYER_HEAD) {
+				m.launchAsyncThread {
+					sender.sendMessage(
+						textComponent {
+							color(NamedTextColor.YELLOW)
+							content("Pegando skin de $owner...")
+						}
+					)
 
-			sender.sendMessage("§aAgora o dono da cabeça do player atual é §b${skullMeta.owner}§a!")
-		} else {
-			sender.sendMessage("§cSegure uma cabeça de um player na sua mão antes de usar!")
+					// É necessário "clonar" o item, se não for "clonado", não será possível usar "meta.owner" caso a skull já tenha
+					// um owner anterior
+					val accountInfo = DreamCore.INSTANCE.skinUtils.retrieveMojangAccountInfo(owner)
+
+					if (accountInfo == null) {
+						sender.sendMessage(
+							textComponent {
+								color(NamedTextColor.RED)
+								content("Conta de $owner não encontrada!")
+							}
+						)
+						return@launchAsyncThread
+					}
+
+					val profileProperty = ProfileProperty(
+						"textures",
+						accountInfo.textures.raw.value,
+						accountInfo.textures.raw.signature,
+					)
+
+					val meta = item.itemMeta as SkullMeta
+					val profile = if (autoUpdate) {
+						Bukkit.createProfile(UUID.fromString(accountInfo.uuid), accountInfo.username)
+					} else {
+						Bukkit.createProfile(UUID(0L, 0L), "")
+					}
+
+					profile.setProperty(profileProperty)
+
+					meta.playerProfile = profile
+					item.itemMeta = meta
+
+					sender.sendMessage("§aAgora a skin da cabeça é a skin do §b${owner}§a na Mojang!")
+				}
+			} else {
+				sender.sendMessage("§cSegure uma cabeça de um player na sua mão antes de usar!")
+			}
 		}
 	}
 }
