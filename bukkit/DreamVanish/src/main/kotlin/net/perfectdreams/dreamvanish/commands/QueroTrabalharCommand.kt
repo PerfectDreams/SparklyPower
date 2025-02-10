2d86c829bf8c28e9ae191b7f5b8f0dac274ebd15
André Victor <60321993+hechfx@users.noreply.github.com>
2025-02-10 11:41:29 -0300

    Allow users to enable/disable vanish/querotrabalhar for others

--- bukkit/DreamVanish/src/main/kotlin/net/perfectdreams/dreamvanish/commands/QueroTrabalharCommand.kt
+++ bukkit/DreamVanish/src/main/kotlin/net/perfectdreams/dreamvanish/commands/QueroTrabalharCommand.kt
@@ -1,24 +1,120 @@
 package net.perfectdreams.dreamvanish.commands
 
-import net.perfectdreams.dreamcore.utils.commands.DSLCommandBase
+import net.kyori.adventure.text.format.NamedTextColor
+import net.perfectdreams.dreamcore.utils.adventure.append
+import net.perfectdreams.dreamcore.utils.adventure.appendCommand
+import net.perfectdreams.dreamcore.utils.commands.context.CommandArguments
+import net.perfectdreams.dreamcore.utils.commands.context.CommandContext
+import net.perfectdreams.dreamcore.utils.commands.declarations.SparklyCommandDeclarationWrapper
+import net.perfectdreams.dreamcore.utils.commands.declarations.sparklyCommand
+import net.perfectdreams.dreamcore.utils.commands.executors.SparklyCommandExecutor
+import net.perfectdreams.dreamcore.utils.commands.options.CommandOptions
 import net.perfectdreams.dreamvanish.DreamVanish
 import net.perfectdreams.dreamvanish.DreamVanishAPI
+import org.bukkit.Bukkit
 
-object QueroTrabalharCommand : DSLCommandBase<DreamVanish> {
-    override fun command(plugin: DreamVanish) = create(
-        listOf("querotrabalhar")
-    ) {
+class QueroTrabalharCommand(val m: DreamVanish) : SparklyCommandDeclarationWrapper {
+    override fun declaration() = sparklyCommand(listOf("querotrabalhar")) {
         permission = "dreamvanish.querotrabalhar"
 
-        executes {
-            if (DreamVanishAPI.isQueroTrabalhar(player)) {
-                DreamVanishAPI.queroTrabalharPlayers.remove(player)
-                sender.sendMessage("§aVocê saiu do modo trabalhar!")
+        executor = QueroTrabalharCommandExecutor()
+    }
+
+    inner class QueroTrabalharCommandExecutor : SparklyCommandExecutor() {
+        inner class Options : CommandOptions() {
+            val playerName = optionalWord("player_name")
+        }
+
+        override val options = Options()
+
+        override fun execute(context: CommandContext, args: CommandArguments) {
+            val playerName = args[options.playerName]
+
+            if (playerName == null) {
+                // If playerName is null, our target is ourselves
+                val player = context.requirePlayer()
+
+                if (DreamVanishAPI.isQueroTrabalhar(player)) {
+                    DreamVanishAPI.queroTrabalharPlayers.remove(player)
+                    context.sendMessage {
+                        append("Você saiu do modo trabalhar!") {
+                            color(NamedTextColor.GREEN)
+                        }
+                    }
+                } else {
+                    DreamVanishAPI.queroTrabalharPlayers.add(player)
+                    context.sendMessage {
+                        append("Você agora está no modo trabalhar!") {
+                            color(NamedTextColor.GREEN)
+                        }
+                        append("\n")
+                        append("Use o ") {
+                            color(NamedTextColor.GRAY)
+                        }
+                        appendCommand("/querotrabalhar")
+                        append(" apenas quando for necessário, você é da Staff, seu trabalho é ajudar os players!") {
+                            color(NamedTextColor.GRAY)
+                        }
+                    }
+                }
             } else {
-                DreamVanishAPI.queroTrabalharPlayers.add(player)
-                sender.sendMessage("§aVocê agora está no modo trabalhar!")
-                sender.sendMessage("")
-                sender.sendMessage("§7Use o §6/querotrabalhar§7 apenas quando for necessário, você é da Staff, seu trabalho é ajudar os players!")
+                // If playerName is not null, our target is another player
+                val player = Bukkit.getPlayer(playerName) ?: run {
+                    context.sendMessage {
+                        append("O jogador ") {
+                            color(NamedTextColor.RED)
+                        }
+                        append(playerName) {
+                            color(NamedTextColor.AQUA)
+                        }
+                        append(" não está online!") {
+                            color(NamedTextColor.RED)
+                        }
+                    }
+                    return
+                }
+
+                if (DreamVanishAPI.isQueroTrabalhar(player)) {
+                    DreamVanishAPI.queroTrabalharPlayers.remove(player)
+
+                    context.sendMessage {
+                        append("§aO jogador ") {
+                            color(NamedTextColor.AQUA)
+                        }
+                        append(playerName) {
+                            color(NamedTextColor.AQUA)
+                        }
+                        append(" §asaiu do modo trabalhar!") {
+                            color(NamedTextColor.GREEN)
+                        }
+                    }
+
+                    player.sendMessage("§aVocê saiu do modo trabalhar!")
+                } else {
+                    DreamVanishAPI.queroTrabalharPlayers.add(player)
+
+                    context.sendMessage {
+                        append("O jogador ") {
+                            color(NamedTextColor.GREEN)
+                        }
+                        append(playerName) {
+                            color(NamedTextColor.AQUA)
+                        }
+                        append(" agora está no modo trabalhar!") {
+                            color(NamedTextColor.GREEN)
+                        }
+                        append("\n")
+                        append("Use o ") {
+                            color(NamedTextColor.GRAY)
+                        }
+                        appendCommand("/querotrabalhar")
+                        append(" apenas quando for necessário, você é da Staff, seu trabalho é ajudar os players!") {
+                            color(NamedTextColor.GRAY)
+                        }
+                    }
+
+                    player.sendMessage("§aVocê agora está no modo trabalhar!")
+                }
             }
         }
     }
--- bukkit/DreamVanish/src/main/kotlin/net/perfectdreams/dreamvanish/commands/VanishCommand.kt
+++ bukkit/DreamVanish/src/main/kotlin/net/perfectdreams/dreamvanish/commands/VanishCommand.kt
@@ -1,22 +1,134 @@
 package net.perfectdreams.dreamvanish.commands
 
-import net.perfectdreams.dreamcore.utils.commands.DSLCommandBase
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+import net.perfectdreams.dreamcore.utils.adventure.append
+import net.perfectdreams.dreamcore.utils.commands.context.CommandArguments
+import net.perfectdreams.dreamcore.utils.commands.context.CommandContext
+import net.perfectdreams.dreamcore.utils.commands.declarations.SparklyCommandDeclarationWrapper
+import net.perfectdreams.dreamcore.utils.commands.declarations.sparklyCommand
+import net.perfectdreams.dreamcore.utils.commands.executors.SparklyCommandExecutor
+import net.perfectdreams.dreamcore.utils.commands.options.CommandOptions
 import net.perfectdreams.dreamvanish.DreamVanish
 import net.perfectdreams.dreamvanish.DreamVanishAPI
+import org.bukkit.Bukkit
 
-object VanishCommand : DSLCommandBase<DreamVanish> {
-    override fun command(plugin: DreamVanish) = create(
-        listOf("vanish")
-    ) {
+class VanishCommand(val m: DreamVanish) : SparklyCommandDeclarationWrapper {
+    override fun declaration() = sparklyCommand(listOf("vanish")) {
         permission = "dreamvanish.vanish"
 
-        executes {
-            if (DreamVanishAPI.isVanished(this.player)) {
-                DreamVanishAPI.setVanishedStatus(this.player, false)
-                player.sendMessage("§aVocê agora está §lvísível§a!")
+        executor = VanishCommandExecutor()
+    }
+
+    inner class VanishCommandExecutor : SparklyCommandExecutor() {
+        inner class Options : CommandOptions() {
+            val playerName = optionalWord("player_name")
+        }
+
+        override val options = Options()
+
+        override fun execute(context: CommandContext, args: CommandArguments) {
+            val playerName = args[options.playerName]
+
+            if (playerName == null) {
+                // If the playerName is null, our target is ourselves
+                val player = context.requirePlayer()
+
+                if (DreamVanishAPI.isVanished(player)) {
+                    DreamVanishAPI.setVanishedStatus(player, false)
+                    context.sendMessage {
+                        append("Você agora está ") {
+                            color(NamedTextColor.GREEN)
+                        }
+
+                        append("visível") {
+                            decorate(TextDecoration.BOLD)
+                            color(NamedTextColor.GREEN)
+                        }
+
+                        append("!") {
+                            color(NamedTextColor.GREEN)
+                        }
+                    }
+                } else {
+                    DreamVanishAPI.setVanishedStatus(player, true)
+                    context.sendMessage {
+                        append("Você agora está ") {
+                            color(NamedTextColor.GREEN)
+                        }
+
+                        append("invisível") {
+                            decorate(TextDecoration.BOLD)
+                            color(NamedTextColor.GREEN)
+                        }
+
+                        append("!") {
+                            color(NamedTextColor.GREEN)
+                        }
+                    }
+                }
             } else {
-                DreamVanishAPI.setVanishedStatus(this.player, true)
-                player.sendMessage("§aVocê agora está §linvisível§a!")
+                // If the playerName is not null, our target is another player
+                val player = Bukkit.getPlayer(playerName) ?: run {
+                    context.sendMessage {
+                        append("O jogador ") {
+                            color(NamedTextColor.RED)
+                        }
+                        append(playerName) {
+                            color(NamedTextColor.AQUA)
+                        }
+                        append(" não está online!") {
+                            color(NamedTextColor.RED)
+                        }
+                    }
+                    return
+                }
+
+                if (DreamVanishAPI.isVanished(player)) {
+                    DreamVanishAPI.setVanishedStatus(player, false)
+                    context.sendMessage {
+                        append("O jogador ") {
+                            color(NamedTextColor.GREEN)
+                        }
+                        append(playerName) {
+                            color(NamedTextColor.AQUA)
+                        }
+                        append(" agora está ") {
+                            color(NamedTextColor.GREEN)
+                        }
+
+                        append("visível") {
+                            decorate(TextDecoration.BOLD)
+                            color(NamedTextColor.GREEN)
+                        }
+
+                        append("!") {
+                            color(NamedTextColor.GREEN)
+                        }
+                    }
+                    player.sendMessage("§aVocê agora está §lvísível§a!")
+                } else {
+                    DreamVanishAPI.setVanishedStatus(player, true)
+                    context.sendMessage {
+                        append("O jogador ") {
+                            color(NamedTextColor.GREEN)
+                        }
+                        append(playerName) {
+                            color(NamedTextColor.AQUA)
+                        }
+                        append(" agora está ") {
+                            color(NamedTextColor.GREEN)
+                        }
+                        append("invisível") {
+                            decorate(TextDecoration.BOLD)
+                            color(NamedTextColor.GREEN)
+                        }
+                        append("!") {
+                            color(NamedTextColor.GREEN)
+                        }
+                    }
+                    player.sendMessage("§aVocê agora está §linvisível§a!")
+                }
             }
         }
     }
